9cc8e2336af39ef76422eaafe410650c39e67223
Jimmy Blackwood <Jimmy Blackwood@DESKTOP-AOM0ABL>
2022-05-15 01:18:20 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_6/HomeTask_6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    int week, day;
+    cout << "Введите количество дней: \n";
+    cin >> day;
+
+    week = day / 7;
+    day = day % 7;
+
+    cout << week << " недель " << day << " дней ";
+
+    return 0;
+}
+
+
